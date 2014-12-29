04a620ef0e0211d87412a58df1a7d71fd4915e9a
zieren <github@zieren.de>
2014-12-29 18:45:51 +0100

    Remove unused logger.

--- client/ipa/metadata.py
+++ client/ipa/metadata.py
@@ -6,9 +6,6 @@ import raspisys
 class Metadata:
   """Provides metadata such as the current time and stratum."""
 
-  def __init__(self):
-    self._log = K.get_logger(K.LOG_NAME_METADATA)
-
   def get_sample(self):
     return K.META_KEY, {# TODO: Avoid subprocess?
                         K.STRATUM_KEY: raspisys.get_stratum(),
