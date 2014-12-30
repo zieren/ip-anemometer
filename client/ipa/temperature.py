22100a48179000a6de6d222ed5d184d034a2422d
zieren <github@zieren.de>
2014-12-30 10:50:34 +0100

    Style.

--- client/ipa/temperature.py
+++ client/ipa/temperature.py
@@ -4,7 +4,7 @@ import raspisys
 
 
 class Temperature:
-  """Provides onboard temperature."""
+  """Provides CPU temperature."""
 
   def get_sample(self):
     # TODO: It's a little awkward that we need to start a subprocess synchronously.
