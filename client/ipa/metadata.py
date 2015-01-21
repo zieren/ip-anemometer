5defab78e4411291e4494f9b9326fa2f0207a74e
zieren <github@zieren.de>
2015-01-21 11:59:32 +0100

    No need to cache md5.

--- client/ipa/metadata.py
+++ client/ipa/metadata.py
@@ -5,10 +5,7 @@ import raspisys
 class Metadata:
   """Provides metadata such as the current time and stratum."""
 
-  def __init__(self):
-    self._md5 = common.client_md5()
-
   def get_sample(self):
     return 'meta', {'stratum': raspisys.get_stratum(),  # TODO: Avoid subprocess?
-                    'md5': self._md5,
+                    'md5': common.client_md5(),
                     'cts': common.timestamp()}
