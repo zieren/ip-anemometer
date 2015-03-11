35eee97c92e688884210a8e4c83995565d9fe2df
zieren <github@zieren.de>
2015-03-11 15:54:57 +0100

    Avoid bogus "failed to sync clock" caused by clock forwarding beyond epoch timeout set based on unsynced clock.

--- client/sh/await_clock_sync.sh
+++ client/sh/await_clock_sync.sh
@@ -5,33 +5,28 @@ source lib.sh
 
 # For information on NTP (e.g. stratum) see http://en.wikipedia.org/wiki/Network_Time_Protocol.
 
+# Arbitrarily require a stratum of 8 or better.
 ACCEPTABLE_STRATUM=8
 CHECK_INTERVAL_SECONDS=5
-RESTART_AFTER_SECONDS=60
-TIMEOUT_SECONDS=180
-
-function set_restart_epoch_seconds() {
-  let "RESTART_EPOCH_SECONDS=$(epoch_seconds) + $RESTART_AFTER_SECONDS"
-}
-
-function set_timeout_epoch_seconds() {
-  let "TIMEOUT_EPOCH_SECONDS=$(epoch_seconds) + $TIMEOUT_SECONDS"
-}
+# The clock might skip, so we count iterations instead.
+RESTART_AFTER_CHECKS=10
+TIMEOUT_AFTER_CHECKS=30
 
 STRATUM=16
-set_restart_epoch_seconds
-set_timeout_epoch_seconds
+i=0
 log "waiting for clock to sync (stratum must be <= $ACCEPTABLE_STRATUM)"
-while [ $(epoch_seconds) -lt $TIMEOUT_EPOCH_SECONDS ]; do
+while [ $i -lt $TIMEOUT_AFTER_CHECKS ]; do
   STRATUM=$(ntpq -c "rv 0 stratum" | sed -r s/.*=//)
-  if [ $STRATUM -le $ACCEPTABLE_STRATUM ]; then
-    log "clock is synced (stratum: $STRATUM)"
+  log "stratum now: $STRATUM"
+  if [ -n "$STRATUM" ] && [ $STRATUM -le $ACCEPTABLE_STRATUM ]; then
+    log "clock is synced"
     exit 0
   fi
-  if [ $(epoch_seconds) -gt $RESTART_EPOCH_SECONDS ]; then
+  let i="$i + 1"
+  let DO_RESTART="$i % $RESTART_AFTER_CHECKS"
+  if [ $DO_RESTART -eq 0 ]; then
     log "restarting ntpd"
     sudo /etc/init.d/ntp restart
-    set_restart_epoch_seconds
   fi
   sleep $CHECK_INTERVAL_SECONDS
 done
