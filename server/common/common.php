6705da5d36d4a68eaf4f409ba257dec46a61b657
zieren <github@zieren.de>
2015-03-22 13:54:12 +0100

    Version 0.1.1.

--- server/common/common.php
+++ server/common/common.php
@@ -32,7 +32,7 @@ function ipaFatalErrorHandler() {
 }
 register_shutdown_function('ipaFatalErrorHandler');
 
-define('IPA_SERVER_HEADING', 'IP anemometer server 0.1.0');
+define('IPA_SERVER_HEADING', 'IP anemometer server 0.1.1');
 
 define('NOT_AVAILABLE', 'n/a');
 
