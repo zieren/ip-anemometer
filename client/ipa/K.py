01d9dc141dcc2ad44abac6cee3cde9af4c549ab2
zieren <github@zieren.de>
2015-08-01 11:29:36 +0200

    Increase client and server version to 0.2.0.

--- client/ipa/K.py
+++ client/ipa/K.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
 
-CLIENT_GREETING = 'IP anemometer client 0.1.1 - (c) 2015 Jörg Zieren - http://zieren.de - GNU GPL v3'
+CLIENT_GREETING = 'IP anemometer client 0.2.0 - (c) 2015 Jörg Zieren - http://zieren.de - GNU GPL v3'
 CONFIG_FILENAME = 'ipa.cfg'
 NOT_AVAILABLE = 'n/a'
--- server/common/common.php
+++ server/common/common.php
@@ -32,7 +32,7 @@ function ipaFatalErrorHandler() {
 }
 register_shutdown_function('ipaFatalErrorHandler');
 
-define('IPA_SERVER_HEADING', 'IP anemometer server 0.1.1');
+define('IPA_SERVER_HEADING', 'IP anemometer server 0.2.0');
 
 define('NOT_AVAILABLE', 'n/a');
 
