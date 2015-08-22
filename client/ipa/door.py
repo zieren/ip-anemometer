e1cdcdc3862310719f0d97e3902209d6c58c247d
zieren <github@zieren.de>
2015-08-22 16:00:34 +0200

    Avoid redundant (i.e. same state as before) door events. Update comments to explain why we accept doing a synchronous read on get_sample.

--- client/ipa/door.py
+++ client/ipa/door.py
@@ -12,14 +12,15 @@ class Door:
   _PIN = C.DOOR_INPUT_PIN()
   _OPEN_STATE = C.DOOR_OPEN_STATE()
 
-  # TODO: Make these configurable?
+  # TODO: Make these configurable? They might depend on cable layout etc.
   _STABLE_READ_INTERVAL_MILLIS = 25
-  _STABLE_READ_COUNT = 20
+  _STABLE_READ_COUNT = 5  # 20 was OK, but I believe sometimes too slow.
 
   def __init__(self):
     self._log = log.get_logger('ipa.door')
     self._lock = threading.Lock()
     self._events = []
+    self._previous_door_open = None
     GPIO.setup(Door._PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
     GPIO.add_event_detect(Door._PIN, GPIO.BOTH, callback=self._read_door_callback,
                           bouncetime=C.DOOR_DEBOUNCE_MILLIS())
@@ -34,21 +35,25 @@ class Door:
     door_open = 1 if read_stable == Door._OPEN_STATE else 0
     self._log.debug('door_open in callback: %d' % door_open)
     with self._lock:
-      self._events.append((common.timestamp(), door_open))
+      self._consider_door_open(door_open)
     return door_open
 
   def get_sample(self):
+    # We don't want to rely on the callback; if we miss one edge (e.g. because the switch is
+    # bouncing) we'd be stuck with an incorrect state until the next callback happens. So we do a
+    # simple read here, accepting that we may be unlucky and hit noise on the cable, but that seems
+    # much less likely that the first case. It would also very likely be corrected in the next call.
+    door_open = 1 if GPIO.input(Door._PIN) == Door._OPEN_STATE else 0
+    self._log.debug('door_open in get_sample: %d' % door_open)
     with self._lock:
+      self._consider_door_open(door_open)
       events = self._events
       self._events = []
-    # TODO: Remove this redundant read if the stable read is really stable :-)
-    door_open = 1 if GPIO.input(Door._PIN) == Door._OPEN_STATE else 0
-    events.append((common.timestamp(), door_open))
-    self._log.debug('door_open in get_sample: %d' % door_open)
-    # TODO: Enable this when it's otherwise working reliably.
-#     # Remove duplicates.
-#     events_compressed = []
-#     for e in events:
-#       if not events_compressed or events_compressed[-1][1] != e[1]:  # compare state (index 1)
-#         events_compressed.append[e]
     return 'door', events
+
+  def _consider_door_open(self, door_open):
+    """Append new door_open state if it differs from the previous state. Must hold lock."""
+    if door_open != self._previous_door_open:
+      self._events.append((common.timestamp(), door_open))
+      self._previous_door_open = door_open
+
