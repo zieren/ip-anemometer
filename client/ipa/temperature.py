e8ddd101b129d0ee09bf6ec6f5755739007c54e2
zieren <github@zieren.de>
2015-08-29 14:21:49 +0200

    Remove a TODO that I'm not going to do.

--- client/ipa/temperature.py
+++ client/ipa/temperature.py
@@ -6,5 +6,4 @@ class Temperature:
   """Provides CPU temperature."""
 
   def get_sample(self):
-    # TODO: It's a little awkward that we need to start a subprocess synchronously.
     return 'temp', (common.timestamp(), raspisys.get_temperature())
