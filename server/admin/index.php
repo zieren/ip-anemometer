820e38d22e7ef8ff8044eaacd459474bc8e0ff40
zieren <github@zieren.de>
2015-10-18 19:08:05 +0200

    Add time+timestamp display (useful for debugging).

--- server/admin/index.php
+++ server/admin/index.php
@@ -30,12 +30,14 @@ if (isset($_POST['clearAll']) && $_POST['confirm']) {
   }
 }
 
+$timestamp = timestamp();
 echo '
 <h1>'.IPA_SERVER_HEADING.'</h1>
 <p>(c) 2015 J&ouml;rg Zieren - <a href="http://zieren.de">zieren.de</a> - GNU GPL v3.
 Components: <a href="http://codefury.net/projects/klogger/">KLogger</a> (c) 2008-2014 Kenny
 Katzgrau, MIT license</p>
-<p><a href="../view/">View anemometer</a></p>
+<p><a href="../view/">View anemometer</a> | Server time: '.formatTimestamp($timestamp)
+.' (epoch millis: '.$timestamp.')</p>
 
 <h2>Configuration</h2>';
 $db->echoConfig();
