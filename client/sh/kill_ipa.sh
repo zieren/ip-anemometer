2619fc8e7cc867653a3d6b88b712921ebfaec1f1
zieren <github@zieren.de>
2016-04-24 17:01:39 +0200

    When IPA isn't running, print message and return 1.

--- client/sh/kill_ipa.sh
+++ client/sh/kill_ipa.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-sudo kill $(ps au | egrep 'ipa_wrapper|python' | grep -v grep | awk '{print $2}')
+PIDS=$(ps au | egrep 'ipa_wrapper|python' | grep -v grep | awk '{print $2}')
+if [ -z "$PIDS" ]; then
+  echo "IPA not running."
+  exit 1
+fi
+sudo kill $PIDS
