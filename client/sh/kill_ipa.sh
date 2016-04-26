f71481745f2e034e1d1c606d7d6e195f6897053e
zieren <github@zieren.de>
2016-04-26 21:07:54 +0200

    Fix ps invocation.

--- client/sh/kill_ipa.sh
+++ client/sh/kill_ipa.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PIDS=$(ps au | egrep 'ipa_wrapper|python' | grep -v grep | awk '{print $2}')
+PIDS=$(ps -ef | egrep 'ipa_wrapper|python' | grep -v grep | awk '{print $2}')
 if [ -z "$PIDS" ]; then
   echo "IPA not running."
   exit 1
