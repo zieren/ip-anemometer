02f33eef3c5e872db6775862577045b9f2103cca
zieren <github@zieren.de>
2017-06-23 16:44:14 +0200

    Fix join_all_threads to do what the comment says.

--- client/ipa/common.py
+++ client/ipa/common.py
@@ -7,13 +7,14 @@ def join_all_threads(timeout_seconds):
   """Join all (non-daemon) threads using the specified total timeout. Returns the number of threads
   failed to join (i.e. 0 on success)."""
   timeout_time = time.time() + timeout_seconds
-  skipped_threads = 0
+  failed_joins = 0
   for t in threading.enumerate():
     if t == threading.current_thread() or t.daemon:
-      skipped_threads += 1
       continue
     t.join(timeout_time - time.time())  # negative values are OK
-  return len(threading.enumerate()) - skipped_threads
+    if t.is_alive():
+      failed_joins += 1
+  return failed_joins
 
 
 def timestamp():
